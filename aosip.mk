605368987e0be688126ef3d85616eba62e7ec4aa
Naomi Calabretta <me@arytonex.pw>
2019-04-02 14:34:51 +0200

    tulip: Add Dirac blobs (as a separate repo)
    * They are in vendor/xiaomi/Dirac
    
    Change-Id: Ie7d3d78a227223a09b57941b722ee5b713dd5eff

--- aosip.mk
+++ aosip.mk
@@ -18,6 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/Dirac/dirac.mk)
 
 # Inherit from lavender device
 $(call inherit-product, device/xiaomi/lavender/device.mk)
