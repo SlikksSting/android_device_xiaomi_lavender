d3f8ebdbf406ddda20a0857b038edbcf854590b1
mdeejay <mdjrussia@gmail.com>
2019-06-11 14:09:28 +0200

    lavender: Switch to XiaomiParts

--- device.mk
+++ device.mk
@@ -176,9 +176,9 @@ PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service \
     android.hardware.broadcastradio@1.0-impl
 
-# Doze
+# Parts
 PRODUCT_PACKAGES += \
-    XiaomiDoze
+    XiaomiParts
 
 # DRM
 PRODUCT_PACKAGES += \
